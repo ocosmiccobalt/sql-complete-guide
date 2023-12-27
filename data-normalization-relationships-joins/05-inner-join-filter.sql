46b40b80c16f4f465b25d5663234b3619b4b0d8a
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-12-27 03:59:10 +0300

    Combine multiple JOINs & filter

---
+++ data-normalization-relationships-joins/05-inner-join-filter.sql
@@ -0,0 +1,6 @@
+SELECT u.id, first_name, last_name, street, house_number, c.name AS city_name
+FROM users AS u
+INNER JOIN addresses AS a ON u.address_id = a.id
+INNER JOIN cities AS c ON a.city_id = c.id
+WHERE c.name = 'Berlin' OR c.name = 'New York'
+ORDER BY u.id DESC;
