18841015291ea7f3b5a6e5e3a21f027fed1feaef
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-12-27 04:29:22 +0300

    Use RIGHT JOIN

--- data-normalization-relationships-joins/06-left-join.sql
+++ data-normalization-relationships-joins/06-left-join.sql
@@ -2,3 +2,8 @@ SELECT *
 FROM addresses AS a
 LEFT JOIN users AS u ON a.id = u.address_id
 LEFT JOIN cities AS c ON c.id = a.city_id;
+
+-- SELECT *
+-- FROM users AS u
+-- RIGHT JOIN addresses AS a ON a.id = u.address_id
+-- LEFT JOIN cities AS c ON c.id = a.city_id;
