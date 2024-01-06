1765ef027a37364a0c4b528e2d9214ff012953df
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-01-07 01:23:58 +0300

    Use MIN() & MAX() (and add aliases)

---
+++ grouping-aggregate-functions/02-max-min.sql
@@ -0,0 +1,5 @@
+-- SELECT MAX(num_seats) FROM tables;
+-- SELECT MIN(num_seats) FROM tables;
+-- SELECT MAX(amount_billed) AS max_billed, MAX(amount_tipped) AS max_tipped
+-- FROM bookings;
+SELECT MIN(booking_date), MAX(booking_date) FROM bookings;
